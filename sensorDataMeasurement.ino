14086dd014ac045d119cff0eb96963f5f051e9ea
cappochinho <edwardprempeh18@gmail.com>
2021-10-22 09:52:48 +0000

    changed Arduino output to JSON

--- sensorDataMeasurement.ino
+++ sensorDataMeasurement.ino
@@ -1,216 +1,41 @@
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#include "ITG3200.h"
 
-//--- Accelerometer Register Addresses
-#define Power_Register 0x2D
-#define X_Axis_Register_DATAX0 0x32 // Hexadecima address for the DATAX0 internal register.
-#define X_Axis_Register_DATAX1 0x33 // Hexadecima address for the DATAX1 internal register.
-#define Y_Axis_Register_DATAY0 0x34 
-#define Y_Axis_Register_DATAY1 0x35
-#define Z_Axis_Register_DATAZ0 0x36
-#define Z_Axis_Register_DATAZ1 0x37
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+ITG3200 gyro;
 
-int ADXAddress = 0x53;  //Device address in which is also included the 8th bit for selecting the mode, read in this case.
-
-int X0,X1,X_out;
-int Y0,Y1,Y_out;
-int Z1,Z0,Z_out;
-float Xa,Ya,Za;
-
-//gyroscope specs
-#define Gyro_gX0 0x28  
-#define Gyro_gX1 0x29
-#define Gyro_gY0 0x2A
-#define Gyro_gY1 0x2B
-#define Gyro_gZ0 0x2C  
-#define Gyro_gZ1 0x2D
-
-int Gyro = 0x69; //Device address in which is also included the 8th bit for selecting the mode, read in this case.
-
-int gX0, gX1, gX_out;
-int gY0, gY1, gY_out;
-int gZ0, gZ1, gZ_out;
-float Xg,Yg,Zg;
-float angleX,angleY,angleZ,angleXc,angleYc,angleZc;
-
-
-unsigned long start, finished, elapsed;
-float dt=0.015;
-
-
-void setup() {
-  Wire.begin(); // Initiate the Wire library    
-  Serial.begin(9600);    
-  delay(100);
+void setup(void) 
+{
+  Serial.begin(9600);
   
-  Wire.beginTransmission(ADXAddress);
-  Wire.write(Power_Register); // Power_CTL Register
-  // Enable measurement
-  Wire.write(8); // Bit D3 High for measuring enable (0000 1000)
-  Wire.endTransmission();
-  
-  Wire.begin();                
-  Serial.begin(9600);    
-  delay(100);
-  
-  Wire.beginTransmission(Gyro);
-  Wire.write(0x20); // CTRL_REG1 - Power Mode
-  Wire.write(15);   // Normal mode: 15d - 00001111b   
-  Wire.endTransmission();
-  
-  Wire.beginTransmission(Gyro);
-  Wire.write(0x23); // CTRL_REG4 - Sensitivity, Scale Selection
-  Wire.write(48);   // 2000dps: 48d - 00110000b
-  Wire.endTransmission();
-  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_16_G);
+
+  gyro.init();
+  gyro.zeroCalibrate(200,10);//sample 200 times to calibrate and it will take 200*10ms
 }
 
-void loop() {
-  // X-axis
-  Wire.beginTransmission(ADXAddress); // Begin transmission to the Sensor 
-  //Ask the particular registers for data
-  Wire.write(X_Axis_Register_DATAX0);
-  Wire.write(X_Axis_Register_DATAX1);  
-  Wire.endTransmission(); // Ends the transmission and transmits the data from the two registers
-  Wire.requestFrom(ADXAddress,2); // Request the transmitted two bytes from the two registers
-  if(Wire.available()<=2) {  // 
-    X0 = Wire.read(); // Reads the data from the register
-    X1 = Wire.read();
-    /* Converting the raw data of the X-Axis into X-Axis Acceleration
-     - The output data is Two's complement 
-     - X0 as the least significant byte
-     - X1 as the most significant byte */ 
-    X1=X1<<8;
-    X_out =X0+X1;
-    Xa=X_out/256.0;// Xa = output value from -1 to +1, Gravity acceleration acting on the X-Axis    
-  }
-
-  // Y-Axis
-  Wire.beginTransmission(ADXAddress); 
-  Wire.write(Y_Axis_Register_DATAY0);
-  Wire.write(Y_Axis_Register_DATAY1);  
-  Wire.endTransmission(); 
-  Wire.requestFrom(ADXAddress,2);
-  if(Wire.available()<=2) { 
-    Y0 = Wire.read();
-    Y1 = Wire.read();
-    Y1=Y1<<8;
-    Y_out =Y0+Y1;
-    Ya=Y_out/256.0;
-  }
-  // Z-Axis
-  Wire.beginTransmission(ADXAddress); 
-  Wire.write(Z_Axis_Register_DATAZ0);
-  Wire.write(Z_Axis_Register_DATAZ1);  
-  Wire.endTransmission(); 
-  Wire.requestFrom(ADXAddress,2);
-  if(Wire.available()<=2) { 
-    Z0 = Wire.read();
-    Z1 = Wire.read();
-    Z1=Z1<<8;
-    Z_out =Z0+Z1;
-    Za=Z_out/256.0;
-  }
-  
-  start=millis();
-  //---- X-Axis
-  Wire.beginTransmission(Gyro); // transmit to device
-  Wire.write(Gyro_gX0);
-  Wire.endTransmission();
-  Wire.requestFrom(Gyro,1); 
-  if(Wire.available()<=1)   
-  {
-    gX0 = Wire.read();
-  }
-  Wire.beginTransmission(Gyro); // transmit to device
-  Wire.write(Gyro_gX1);
-  Wire.endTransmission();
-  Wire.requestFrom(Gyro,1); 
-  if(Wire.available()<=1)   
-  {
-    gX1 = Wire.read();
-  }
-
-  //---- Y-Axis
-  Wire.beginTransmission(Gyro); // transmit to device
-  Wire.write(Gyro_gY0);
-  Wire.endTransmission();
-  Wire.requestFrom(Gyro,1); 
-  if(Wire.available()<=1)   
-  {
-    gY0 = Wire.read();
-  }
-  Wire.beginTransmission(Gyro); // transmit to device
-  Wire.write(Gyro_gY1);
-  Wire.endTransmission();
-  Wire.requestFrom(Gyro,1); 
-  if(Wire.available()<=1)   
-  {
-    gY1 = Wire.read();
-  }
-  
-  //---- Z-Axis
-  Wire.beginTransmission(Gyro); // transmit to device
-  Wire.write(Gyro_gZ0);
-  Wire.endTransmission();
-  Wire.requestFrom(Gyro,1); 
-  if(Wire.available()<=1)   
-  {
-    gZ0 = Wire.read();
-  }
-  Wire.beginTransmission(Gyro); // transmit to device
-  Wire.write(Gyro_gZ1);
-  Wire.endTransmission();
-  Wire.requestFrom(Gyro,1); 
-  if(Wire.available()<=1)   
-  {
-    gZ1 = Wire.read();
-  }
-  
-  //---------- X - Axis
-  
-  // Raw Data
-  gX1=gX1<<8;
-  gX_out =gX0+gX1;
-  
-  // From the datasheet: 70 mdps/digit
-  Xg=gX_out*0.07; // Angular rate
-  // Angular_rate * dt = angle
-  angleXc = Xg*dt;
-  angleX = angleX + angleXc;
-
-  //---------- Y - Axis
-  gY1=gY1<<8;
-  gY_out =gY0+gY1;
-  Yg=gY_out*0.07;
-  angleYc = Yg*dt;
-  angleY = angleY + angleYc;
-  
-  //---------- Z - Axis
-  gZ1=gZ1<<8;
-  gZ_out =gZ0+gZ1;
-  Zg=gZ_out*0.07;
-  angleZc = Zg*dt;
-  angleZ = angleZ + angleZc;
-
-  // Prints the data on the Serial Monitor
-  Serial.print(Xa);
-  Serial.print(",");
-  Serial.print(Ya);
-  Serial.print(",");
-  Serial.print(Za);
-  Serial.print(",");
-  Serial.print(angleX);
-  Serial.print(",");
-  Serial.print(angleY);
-  Serial.print(",");
-  Serial.println(angleZ);
-  
-  delay(10);
-  // Calculating dt
-  finished=millis();
-  elapsed=finished-start;
-  dt=elapsed/1000.0;
-  start = elapsed = 0;
-
-  delay(1000);
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  float ax,ay,az;
+  gyro.getAngularVelocity(&ax,&ay,&az);
+    
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("{\"acc_x\":"); Serial.print(event.acceleration.x); Serial.print(','); Serial.print(event.acceleration.y); Serial.print(','); Serial.print(event.acceleration.z);
+  Serial.print(',');Serial.print(ax); Serial.print(','); Serial.print(ay); Serial.print(','); Serial.println(az);
+  delay(25);
 }
